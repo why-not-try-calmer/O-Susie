4203c494857b146e186c802443dc41130baa8c94
Nycticorax <mrnycticorax@gmail.com>
2021-09-22 16:01:59 +0200

    started clean

---
+++ bot.py
@@ -0,0 +1,102 @@
+from verify import *
+from aiogram import Bot, Dispatcher, types
+from aiogram.dispatcher.webhook import get_new_configured_app
+from datetime import datetime
+from os import environ as env
+from dotenv import load_dotenv
+from urllib.parse import urljoin
+
+load_dotenv()
+TOKEN = env["TELEGRAM_BOT_TOKEN"]
+WEBHOOK_HOST = env["WEBHOOK_HOST"]
+WEBHOOK_URL_PATH = env["WEBHOOK_ENDPOINT"] + TOKEN
+WEBHOOK_URL = urljoin(WEBHOOK_HOST, WEBHOOK_URL_PATH)
+KEY = env["KEY"]
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot)
+
+""" 
+|- LOGGING -|
+import logging
+logging.basicConfig(level=logging.INFO)
+from aiogram.contrib.middlewares.logging import LoggingMiddleware
+dp.middleware.setup(LoggingMiddleware())
+"""
+
+
+@dp.callback_query_handler()
+async def pressed_verification_button(cb: types.CallbackQuery) -> None:
+    user_id = cb.from_user.id
+    if not user_id in PURGATORY:
+        return
+
+    chat_id = cb.message.chat.id
+    key = cb.data
+
+    if key == KEY:
+        replying = bot.send_message(
+            chat_id=chat_id,
+            text=f"Welcome, _{cb.from_user.mention}_! Have a lot of fun!",
+            parse_mode="Markdown"
+        )
+        await asyncio.gather(replying, to_heavens(chat=cb.message.chat, user_id=user_id))
+
+    elif visit_purgatory(user_id):
+        text = "Wrong answer. Make sure you get it right now or you will get banned *forever*."
+        response_msg = await bot.send_message(chat_id=chat_id, text=text, parse_mode="Markdown")
+        delete_after_delay(message_id=response_msg.message_id,
+                           chat=response_msg.chat, delay=DELAY+10)
+
+    else:
+        await to_hell(cb, user_id)
+
+
+@dp.message_handler(content_types=types.ContentTypes.NEW_CHAT_MEMBERS)
+async def just_joined(message: types.Message) -> None:
+    chat = message.chat
+    _values = [getattr(u, "_values") for u in message.new_chat_members]
+    users_ids = [u["id"] for u in _values if not u["is_bot"]]
+
+    response_msg = await bot.send_message(
+        chat_id=message.chat.id,
+        text=f"Hey, a new gecko! If you are not a bot, please answer this question within the next *{DELAY} seconds*. What emoji below resembles the openSUSE mascot the most?",
+        parse_mode="Markdown",
+        reply_markup=create_verification_keyboard()
+    )
+    delete_after_delay(message_id=response_msg.message_id,
+                       chat=response_msg.chat, delay=DELAY+10)
+
+    for task in asyncio.as_completed([restrict(chat, _id) for _id in users_ids]):
+        user_id = await task
+        PURGATORY[user_id] = {"joined_at": datetime.now(), "counter": 0}
+        kick_after_delay(bot, chat, user_id)
+
+
+@dp.message_handler()
+async def handle_messages(message: types.Message) -> None:
+    """ Because the bot might come online after the user has had time to send a couple of messages. """
+    if message.from_user.id in PURGATORY:
+        await bot.delete_message(chat_id=message.chat.id, message_id=message.message_id)
+
+
+async def on_startup(_app) -> None:
+    """Simple hook for aiohttp application which manages webhook"""
+    await bot.delete_webhook()
+    await bot.set_webhook(WEBHOOK_URL)
+
+
+if __name__ == '__main__':
+    from sys import argv
+    if len(argv) < 2 or argv[1] == "--webhook":
+        from aiohttp import web
+        print(f"Called with {argv}, running as webhook")
+        app = get_new_configured_app(dispatcher=dp, path=WEBHOOK_URL_PATH)
+        app.on_startup.append(on_startup)
+        print(f"Set up webhook at {WEBHOOK_URL}, running now")
+        web.run_app(app, host='0.0.0.0', port=int(env.get('PORT', 8080)))
+    else:
+        import uvloop
+        print(f"Called with {argv}, running as long-poller")
+        asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
+        asyncio.run(dp.start_polling())
---
+++ verify.py
@@ -0,0 +1,90 @@
+from datetime import timedelta, datetime
+from functools import reduce
+from typing import List, Tuple
+from random import sample
+from aiogram import types
+from aiogram.bot.bot import Bot
+from aiogram.types.inline_keyboard import InlineKeyboardButton, InlineKeyboardMarkup
+import asyncio
+
+emojis = {
+    "robot": "\U0001F916",
+    "snake": "\U0001F40D",
+    "alien": "\U0001F47D",
+    "gecko": "\U0001F98E",
+    "clown": "\U0001F921",
+    "shark": "\U0001F988"
+}
+
+DELAY = 120
+DELTA = timedelta(seconds=DELAY)
+PURGATORY = {}
+
+
+def list_captcha_randomly() -> List[Tuple[str, str]]:
+    return sample(list(emojis.items()), len(emojis))
+
+
+def rows_of_3(buttons: List[InlineKeyboardButton]) -> List[List[InlineKeyboardButton]]:
+    def reducer(acc, val):
+        if acc and len(acc[-1]) < 3:
+            acc[-1].append(val)
+            return acc
+        acc.append([val])
+        return acc
+    return reduce(reducer, buttons, [])
+
+
+def create_verification_keyboard() -> types.InlineKeyboardMarkup:
+    buttons = [InlineKeyboardButton(text=v, callback_data=k)
+               for k, v in list_captcha_randomly()]
+    keyboard: List[List[InlineKeyboardButton]
+                   ] = rows_of_3(buttons)
+    return InlineKeyboardMarkup(3, inline_keyboard=keyboard)
+
+
+async def restrict(chat: types.Chat, user_id: int) -> int:
+    await chat.restrict(user_id, permissions=types.ChatPermissions(False, False, False, False, False, False, False, False))
+    return user_id
+
+
+async def unrestrict(chat: types.Chat, user_id: int) -> int:
+    await chat.restrict(user_id, permissions=types.ChatPermissions(True, True, True, True, False, False, False, False))
+    return user_id
+
+
+def delete_after_delay(message_id: int, chat: types.Chat, delay: int) -> None:
+    async def deleting(message_id):
+        await asyncio.sleep(delay)
+        await chat.delete_message(message_id)
+        print(f"Message {message_id} in chat {chat} deleted!")
+    asyncio.create_task(deleting(message_id))
+
+
+def kick_after_delay(bot: Bot, chat: types.Chat, user_id: int) -> None:
+    async def kicking(user_id: int) -> None:
+        await asyncio.sleep(DELAY)
+        reply = bot.send_message(
+            chat_id=chat.id, text=f"Time elapsed, kicked {user_id}")
+        kick = chat.kick(user_id)
+        await asyncio.gather(reply, kick)
+    PURGATORY[user_id]["timer"] = asyncio.create_task(kicking(user_id))
+
+
+def visit_purgatory(user_id: int) -> bool:
+    if PURGATORY[user_id]["counter"] == 0 and datetime.now() - PURGATORY[user_id]["joined_at"] < DELTA:
+        PURGATORY[user_id]["counter"] += 1
+        return True
+    return False
+
+
+async def to_heavens(chat: types.Chat, user_id: int) -> None:
+    PURGATORY[user_id]["timer"].cancel()
+    PURGATORY.__delitem__(user_id)
+    await unrestrict(chat=chat, user_id=user_id)
+
+
+async def to_hell(cb: types.CallbackQuery, user_id: int) -> None:
+    PURGATORY[user_id]["timer"].cancel()
+    PURGATORY.__delitem__(user_id)
+    await cb.message.chat.kick(user_id)
